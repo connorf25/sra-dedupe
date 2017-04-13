305bfcd463da9c95b35ea24756ed63c455154811
Matt Carter <m@ttcarter.com>
2017-04-13 12:19:28 +1000

    Added test for obvious duplicates

--- test/compareAll.js
+++ test/compareAll.js
@@ -13,6 +13,26 @@ describe('dedupe.compareAll()', function() {
 			.on('end', done);
 	});
 
+	it('should compare a library with obvious duplicates', function(done) {
+		this.timeout(10 * 10000);
+		var library = require('./data/dupes-obvious.json');
+		var dedupe = dedupeLib();
+		var dupes = [];
+		var progressUpdates = 0;
+		dedupe.compareAll(library)
+			.on('dupe', function(ref1, ref2, result) {
+				dupes.push(result);
+			})
+			.on('progress', function(current, max) {
+				progressUpdates++;
+			})
+			.on('end', function() {
+				expect(progressUpdates).to.be.above(0);
+				expect(dupes).to.have.length(100);
+				done();
+			});
+	});
+
 	it('should compare a library with duplicates', function(done) {
 		this.timeout(10 * 10000);
 		var library = require('./data/dupes.json');
